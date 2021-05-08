b2e4c67b01e21fc95c7126b3f0d17f985874dda5
NIkola-mkd <58082595+NIkola-mkd@users.noreply.github.com>
2021-05-08 23:12:44 +0200

    link correction

--- scripts/logout.js
+++ scripts/logout.js
@@ -3,7 +3,7 @@
 function logOut() {
     let logout = confirm("Do you want to log out ?");
     if(logout==true){
-        document.location.href='menu/signup.html';
+        document.location.href='menu/login.html';
         return true;
     }else{
         return false;
